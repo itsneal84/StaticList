2c6c80ce12f72ca1c9b4e2c6e5445e9331b0e3a8
itsneal84 <35348532+itsneal84@users.noreply.github.com>
2019-01-21 16:53:49 +0000

    Add files via upload

---
+++ StaticList/CustomArrayListTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticList
+{
+    class CustomArrayListTest
+    {
+        static void Main()
+        {
+            CustomArrayList<string> shoppingList =
+            new CustomArrayList<string>();
+            shoppingList.Add("Milk");
+            shoppingList.Add("Honey");
+            shoppingList.Add("Olives");
+            shoppingList.Add("Water");
+            shoppingList.Add("Beer");
+            shoppingList.Remove("Olives");
+            shoppingList.Insert(1, "Fruits");
+            shoppingList.Insert(0, "Cheese");
+            shoppingList.Insert(6, "Vegetables");
+            shoppingList.RemoveAt(0);
+            shoppingList[3] = "A lot of " + shoppingList[3];
+            Console.WriteLine("We need to buy:");
+            for (int i = 0; i < shoppingList.Count; i++)
+            {
+                Console.WriteLine(" - " + shoppingList[i]);
+            }
+            Console.WriteLine("Position of 'Beer' = {0}",
+            shoppingList.IndexOf("Beer"));
+            Console.WriteLine("Position of 'Water' = {0}",
+            shoppingList.IndexOf("Water"));
+            Console.WriteLine("Do we have to buy Bread? " +
+                shoppingList.Contains("Bread"));
+
+            Console.ReadKey();
+        }
+    }
+}
